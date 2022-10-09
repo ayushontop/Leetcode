117839468d44c17d3f57d69cf31eb3a4521412a8
Ayush Tiwari <55987406+ayushontop@users.noreply.github.com>
2022-10-09 22:37:51 +0530

    Time: 4 ms (47.42%), Space: 42.5 MB (38.39%) - LeetHub

---
+++ 1309-decrypt-string-from-alphabet-to-integer-mapping/1309-decrypt-string-from-alphabet-to-integer-mapping.java
@@ -0,0 +1,35 @@
+class Solution {
+    public String freqAlphabets(String s) {
+        String j= s.replace("10#","j");
+        String k= j.replace("11#","k");
+        String l= k.replace("12#","l");
+        String m= l.replace("13#","m");
+        String n= m.replace("14#","n");
+        String o= n.replace("15#","o");
+        String p= o.replace("16#","p");
+        String q= p.replace("17#","q");
+        String r= q.replace("18#","r");
+        String s2= r.replace("19#","s");
+        String t= s2.replace("20#","t");
+        String u= t.replace("21#","u");
+        String v= u.replace("22#","v");
+        String w= v.replace("23#","w");
+        String x= w.replace("24#","x");
+        String y= x.replace("25#","y");
+        String z= y.replace("26#","z");
+        String a= z.replace("1","a");
+        String b= a.replace("2","b");
+        String c= b.replace("3","c");
+        String d= c.replace("4","d");
+        String e= d.replace("5","e");
+        String f= e.replace("6","f");
+        String g= f.replace("7","g");
+        String h= g.replace("8","h");
+        String i= h.replace("9","i");
+     
+        return i;
+        
+        
+        
+    }
+}
